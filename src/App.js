6f7286ed23c085e391f219ec624afcb77e70c637
RoseKimball <mrkimbal@usc.edu>
2020-08-20 20:41:55 -0700

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderNav from './Containers/HeaderNav/HeaderNav';
+import SideBar from './Containers/SideBar/SideBar';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav/>
+      <SideBar/>
     </div>
   );
-}
+};
 
 export default App;
